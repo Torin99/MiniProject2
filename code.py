ad95fae1e68804407da8da282721de8298e615b6
grantw13 <78278076+grantw13@users.noreply.github.com>
2023-11-12 14:30:53 -0500

    Update code.py

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ vectorizer = TfidfVectorizer(analyzer = 'word', max_features = 5000)
 vectorizer.fit(train_data)
 processed_train_data =  vectorizer.transform(train_data)
 processed_test_data =  vectorizer.transform(test_data)
-"""
+
 for estimators in [3,10,50,100,300,500,1000,2000]:
     boost = AdaBoostClassifier(n_estimators = estimators, learning_rate = 0.5)
     model = boost.fit(processed_train_data, train_labels)
@@ -25,7 +25,7 @@ for alpha in [0.1,0.5,0.8,1,1.5,2,3]: #note that from 2 onwards produces the exa
     y_pred = model.predict(processed_test_data)
 
     print("Accuracy with learning rate:", alpha, "is:", metrics.accuracy_score(y_pred, test_labels))
-"""
+
 boost = AdaBoostClassifier(n_estimators = 1000, learning_rate = 0.5)
 model = boost.fit(processed_train_data, train_labels)
 
@@ -41,41 +41,43 @@ print(40*'=')
 print("Testing 20newsgroups Dataset")
 print(40*'=')
 
-twenty_train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'))
+categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
 
-train_data = twenty_train["data"]
-train_labels = twenty_train["target"]
+twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
 
-twenty_test = fetch_20newsgroups(subset="test", remove=("headers", "footers", "quotes"))
+train_data = twenty_train.data
+train_labels = twenty_train.target
 
-test_data = twenty_test["data"]
-test_labels = twenty_test["target"]
+twenty_test = fetch_20newsgroups(subset="test", categories=categories, shuffle=True, random_state=42)
 
-vectorizer = TfidfVectorizer(analyzer = 'word', max_features = 5000)
-processed_train_data = vectorizer.fit_transform(train_data)
-processed_test_data = vectorizer.fit_transform(test_data)
-"""
+test_data = twenty_test.data
+test_labels = twenty_test.target
+
+vectorizer = TfidfVectorizer(analyzer = 'word')
+X_train = vectorizer.fit_transform(train_data)
+X_test = vectorizer.transform(test_data)
+
 for estimators in [3,10,50,100,300,500,1000,2000]:
-    boost = AdaBoostClassifier(n_estimators = estimators, learning_rate = 0.8)
-    model = boost.fit(processed_train_data, train_labels)
+    boost = AdaBoostClassifier(n_estimators = estimators, learning_rate = 0.5)
+    model = boost.fit(X_train, train_labels)
 
-    y_pred = model.predict(processed_test_data)
+    y_pred = model.predict(X_test)
 
-    print("Accuracy with number of estimators", estimators, "is:", metrics.accuracy_score(y_pred, test_labels))
+    print("Accuracy with number of estimators", estimators, "is:", metrics.accuracy_score(test_labels, y_pred))
 
-for alpha in [0.1,0.5,0.8,1,1.5,2,3]: #note that from 2 onwards produces the exact same accuracy
+for alpha in [0.1,0.5,0.8,1,1.5,2,3]: 
     boost = AdaBoostClassifier(n_estimators = 300, learning_rate = alpha)
-    model = boost.fit(processed_train_data, train_labels)
+    model = boost.fit(X_train, train_labels)
 
-    y_pred = model.predict(processed_test_data)
+    y_pred = model.predict(X_test)
 
-    print("Accuracy with learning rate:", alpha, "is:", metrics.accuracy_score(y_pred, test_labels))
-"""
-boost = AdaBoostClassifier(n_estimators = 300, learning_rate = 0.8)
-model = boost.fit(processed_train_data, train_labels)
-y_pred = model.predict(processed_test_data)
+    print("Accuracy with learning rate:", alpha, "is:", metrics.accuracy_score(test_labels, y_pred))
 
-print("Accuracy:", metrics.accuracy_score(y_pred, test_labels))
-print("Precision:", metrics.precision_score(y_pred, test_labels, average='weighted'))
-print("Recall:", metrics.recall_score(y_pred, test_labels, average='weighted'))
-print("F1 Score:", metrics.f1_score(y_pred, test_labels, average='weighted'))
+ada = AdaBoostClassifier(n_estimators = 300, learning_rate = 0.5)
+ada.fit(X_train, train_labels)
+y_pred = ada.predict(X_test)
+
+print("Accuracy:", metrics.accuracy_score(test_labels, y_pred))
+print("Precision:", metrics.precision_score(test_labels, y_pred, average='weighted'))
+print("Recall:", metrics.recall_score(test_labels, y_pred, average='weighted'))
+print("F1 Score:", metrics.f1_score(test_labels, y_pred, average='weighted'))
